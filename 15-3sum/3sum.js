f024efac52f17eca271d27e38c61100d8f8ca7fb
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-08-13 14:04:47 +0530

    Time: 35 ms (69.93%) | Memory: 72.8 MB (37.08%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    let n = nums.length;
+    let ans = [];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < n; i++) {
+        if (i !== 0 && nums[i] === nums[i - 1])
+            continue;
+        let j = i + 1, k = n - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
+                j++;
+            else if (sum > 0)
+                k--;
+            else {
+                let temp = [nums[i], nums[j], nums[k]];
+                ans.push(temp);
+                j++;
+                k--;
+                while (j < k && nums[j] === nums[j - 1])
+                    j++;
+                while (j < k && nums[k] === nums[k + 1])
+                    k--;
+            }
+        }
+    }
+    return ans;
+};
