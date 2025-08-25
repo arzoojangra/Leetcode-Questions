eed747bb14790883f8043fb49f816e4bd64ba910
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-08-25 21:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (51.79%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+    let n = nums.length;
+    let low = 0, high = n - 1, ans = Infinity;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[low] <= nums[mid]) {
+            ans = Math.min(ans, nums[low]);
+            low = mid + 1;
+        } else {
+            ans = Math.min(ans, nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+};
