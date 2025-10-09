f6b418418cdd969c1467947fe0684efa258fe3ef
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-10-09 17:18:23 +0530

    Time: 410 ms (0.00%) | Memory: 56.7 MB (100.00%) - LeetSync

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} skill
+ * @param {number[]} mana
+ * @return {number}
+ */
+var minTime = function (skill, mana) {
+    const n = skill.length,
+        m = mana.length;
+    const times = new Array(n).fill(0);
+
+    for (let j = 0; j < m; j++) {
+        let curTime = 0;
+        for (let i = 0; i < n; i++) {
+            curTime = Math.max(curTime, times[i]) + skill[i] * mana[j];
+        }
+        times[n - 1] = curTime;
+        for (let i = n - 2; i >= 0; i--) {
+            times[i] = times[i + 1] - skill[i + 1] * mana[j];
+        }
+    }
+    return times[n - 1];
+};
