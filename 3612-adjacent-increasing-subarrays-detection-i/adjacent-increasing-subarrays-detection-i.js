b261d7edd1a0350480aa5f020b88e9fc8266d3b5
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-10-14 21:19:06 +0530

    Time: 61 ms (55.36%) | Memory: 55.4 MB (98.21%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasIncreasingSubarrays = function (nums, k) {
+    const n = nums.length;
+    let cnt = 1,
+        precnt = 0,
+        ans = 0;
+    for (let i = 1; i < n; ++i) {
+        if (nums[i] > nums[i - 1]) {
+            ++cnt;
+        } else {
+            precnt = cnt;
+            cnt = 1;
+        }
+        ans = Math.max(ans, Math.min(precnt, cnt));
+        ans = Math.max(ans, Math.floor(cnt / 2));
+    }
+    return ans >= k;
+};
