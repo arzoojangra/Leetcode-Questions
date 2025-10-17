4367615637ba1434b151f19b2c5d6a313b9e492e
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-10-17 08:41:40 +0530

    Time: 33 ms (77.78%) | Memory: 71.3 MB (83.33%) - LeetSync

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} value
+ * @return {number}
+ */
+var findSmallestInteger = function (nums, value) {
+    const mp = new Array(value).fill(0);
+    for (let x of nums) {
+        const v = ((x % value) + value) % value;
+        mp[v]++;
+    }
+    let mex = 0;
+    while (mp[mex % value] > 0) {
+        mp[mex % value]--;
+        mex++;
+    }
+    return mex;
+};
