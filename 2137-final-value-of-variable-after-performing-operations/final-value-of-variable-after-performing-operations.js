f737c585a0000702583b7946bb3483d6972787a1
Arzoo Jangra <96476616+arzoojangra@users.noreply.github.com>
2025-10-20 16:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 56.7 MB (10.69%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function (operations) {
+    let x = 0;
+    for (const operation of operations) {
+        if (operation === '++X' || operation === 'X++')
+            x++;
+        else x--;
+    }
+    return x;
+};
